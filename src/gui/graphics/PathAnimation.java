439e49babb375d4da99798a5633802f73d345ff0
HonzaBoh <honza.boh@gmail.com>
2026-03-16 11:08:23 +0100

    Animations

---
+++ src/gui/graphics/PathAnimation.java
@@ -0,0 +1,92 @@
+package gui.graphics;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class PathAnimation extends JPanel {
+
+    private Path2D shape;
+
+    private double x = 100;
+    private double y = 300;
+
+    private double angle = 0;
+
+    private double dx = 3;
+
+    public PathAnimation() {
+
+        setBackground(Color.WHITE);
+
+        createShape();
+
+        Timer timer = new Timer(16, e -> updateAnimation());
+        timer.start();
+    }
+
+    private void createShape() {
+
+        shape = new Path2D.Double();
+
+        shape.moveTo(-30, 30);
+        shape.lineTo(0, -40);
+        shape.lineTo(30, 30);
+        shape.closePath();
+    }
+
+    private void updateAnimation() {
+
+        // pohyb
+        x += dx;
+
+        // bounce - uz zname
+        if (x > getWidth() - 50 || x < 50) {
+            dx = -dx;
+        }
+
+        // rotace
+        angle += 0.05;
+
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON
+        );
+
+        // Zapamatovat si puvodni stav
+        AffineTransform old = g2.getTransform();
+        // pohyb smerem
+        g2.translate(x, y);
+
+        // rotace, samotna je jen sama nad sebou
+        g2.rotate(angle);
+
+        g2.setColor(Color.BLUE);
+        g2.fill(shape);
+        g2.setColor(Color.BLACK);
+        g2.draw(shape);
+
+        //puvodni stav
+        g2.setTransform(old);
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Showcase - animace");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.add(new PathAnimation());
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
